b6cbd07c38ed43b22f959281e3a38095f7b57f79
Miguelito-Uzumaki <119423772+Miguelito-Uzumaki@users.noreply.github.com>
2023-02-09 15:13:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var Welcome = "Bem Vindo"
+var Explicação = "Este site é um projeto da minha escola de programação ,"
+var Explicação2 = "o meu nome é Miguel mas pode me chamar de Miguelito ,"
+var Explicação3 = " este site não esta completo então..."
+console.log(Welcome)
+console.log(Explicação)
+console.log(Explicação2)
+console.log(Explicação3)
